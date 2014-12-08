077bd359b0c66c08cd6945e65c4928d5f37da496
Ali <ali54@live.in>
2014-12-08 13:31:38 +0530

    azure test

--- test.php
+++ test.php
@@ -1,13 +1,4 @@
 <?php
-function generateRandomString($length = 8) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-echo generateRandomString();
+echo "azure";
 
 ?>
